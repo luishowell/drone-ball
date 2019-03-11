914f8fc8971b3a76fafb326d9b2fa4c996ba6c08
Joe Walker <joew2410@gmail.com>
2019-03-11 21:20:10 +0000

    added image rotation and crop to specified size

--- android-app/app/src/main/java/com/example/ledsphereapp/ImageSelector.java
+++ android-app/app/src/main/java/com/example/ledsphereapp/ImageSelector.java
@@ -7,17 +7,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.EditText;
 import android.content.Intent;
+import com.soundcloud.android.crop.Crop;
+
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
 
 public class ImageSelector extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
     ImageView imageView2;
+    EditText editImHeight;
+    EditText editImWidth;
     float rotateAngle = 0;
+
+    int image_width = 200;
+    int image_height = 144;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +53,40 @@ public class ImageSelector extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         imageView2 = (ImageView) findViewById(R.id.imageView2);
+
+        editImWidth = (EditText) findViewById(R.id.editImWidth);
+        editImWidth.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                    //store the value in the relevant int value
+                    String image_width_str = v.getText().toString();
+                    image_width = Integer.parseInt(image_width_str);
+                }
+                return handled;
+            }
+        });
+
+
+        editImHeight = (EditText) findViewById(R.id.editImHeight);
+        editImHeight.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    //store the value in the relevant int value
+                    String image_height_str = v.getText().toString();
+                    image_height = Integer.parseInt(image_height_str);
+                }
+                return handled;
+            }
+        });
+
+
+
 
         Button gallery_button = (Button) findViewById(R.id.gallery_button);
         gallery_button.setOnClickListener(new View.OnClickListener() {
@@ -67,19 +117,43 @@ public class ImageSelector extends AppCompatActivity {
         });
 
 
+
     }
 
     private void openGallery() {
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(gallery, PICK_IMAGE);
+        //Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        //startActivityForResult(gallery, PICK_IMAGE);
+        imageView2.setImageDrawable(null);
+        Crop.pickImage((this));
     }
 
     @Override
     protected void  onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+        //if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+        if(requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK){
+            //imageUri = data.getData();
+            beginCrop(data.getData());
+            //imageView2.setImageURI(imageUri);
+            //imageView2.setImageURI(Crop.getOutput(data));
+        } else if (requestCode == Crop.REQUEST_CROP){
+            handleCrop(resultCode, data);
+        } else if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
             imageUri = data.getData();
             imageView2.setImageURI(imageUri);
+        }
+    }
+
+    private void beginCrop(Uri source){
+        Uri outputUri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+        new Crop(source).output(outputUri).withFixedSize(image_width,image_height).start(this);
+    }
+
+    private void handleCrop(int resultCode, Intent result) {
+        if (resultCode == RESULT_OK) {
+            imageView2.setImageURI(Crop.getOutput(result));
+        } else if (resultCode == Crop.RESULT_ERROR) {
+            Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
