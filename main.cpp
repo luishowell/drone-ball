b35dbea91821793590ad5a4ee63c4c651ab6b5a8
Luis Howell <luisbhowell@gmail.com>
2019-03-14 21:54:38 +0000

    added motor driving to main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "SDBlockDevice.h"	//SD card
 #include "bitmap_image.hpp"	//bitmap lib
 #include "apa102.h"	//LED strip driver#
+#include "stepper.hpp"
 
 #define STRIP_LENGTH 144
 #define DISPLAY_STEPS 200
@@ -14,11 +15,6 @@ unsigned int freq = 32000000;
 Serial pc(USBTX, USBRX); // tx, rx
 
 Timer t;
-
-Thread thread1;
-Thread thread2;
-Thread thread3;
-Thread thread4;
 
 apa102 strip_1(PA_5, PA_7, PA_6, STRIP_LENGTH);	//sclk, mosi, miso SPI1
 apa102 strip_2(PB_13, PB_15, PC_2, STRIP_LENGTH);	//sclk, mosi, miso SPI2
@@ -73,28 +69,6 @@ void setup_strips(int level, unsigned int freq)
 }
 
 
-void strip_1_thread()
-{
-	convert_pixels(strip_1_array, 0);
-	strip_1.post(strip_1_array);	
-}
-void strip_2_thread()
-{
-	convert_pixels(strip_2_array, 0);
-	strip_2.post(strip_2_array);	
-}
-void strip_3_thread()
-{
-	convert_pixels(strip_3_array, 0);
-	strip_3.post(strip_3_array);	
-}
-void strip_4_thread()
-{
-	convert_pixels(strip_4_array, 0);
-	strip_4.post(strip_4_array);	
-}
-
-
 int main()
 {
 	pc.printf("\nOpening input2.bmp\n");	
@@ -109,35 +83,39 @@ int main()
 	}
 
 	setup_strips(level, freq);
+
+    Stepper stepper_motor(200, PD_4, PD_5, PD_6, PD_7);
+    stepper_motor.setSpeed(350);  //rpm
 	
+	int display_counter[STRIP_NUMBER] = {0,0,0,0};
 
-	t.start();
-	convert_pixels(strip_1_array, 0);
-	strip_1.post(strip_1_array);
-	convert_pixels(strip_2_array, 0);
-	strip_2.post(strip_2_array);
-	convert_pixels(strip_3_array, 0);
-	strip_3.post(strip_3_array);
-	convert_pixels(strip_4_array, 0);
-	strip_4.post(strip_4_array);
-    t.stop();
-    pc.printf("The time taken for serial was %f seconds\n", t.read());
-	t.reset();
+	while(1)
+	{
+		if (display_counter[STRIP_NUMBER]==DISPLAY_STEPS)
+		{
+			display_counter[0]=0;
+		}
+		for (int i=1; i<STRIP_NUMBER; i++)
+		{
+			display_counter[i] = display_counter[0] + ((DISPLAY_STEPS/4)*i);
+			if (display_counter[i]>=DISPLAY_STEPS)
+			{
+				display_counter[i] = display_counter[i] - DISPLAY_STEPS;
+			}
+		}
 
-	t.start();
-	thread1.start(strip_1_thread);
-	thread2.start(strip_2_thread);
-	thread3.start(strip_3_thread);
-	thread4.start(strip_4_thread);
-	thread1.join();
-	thread2.join();
-	thread3.join();
-	thread4.join();
-    t.stop();
-    pc.printf("The time taken for threads was %f seconds\n", t.read());
-	t.reset();
+		convert_pixels(strip_1_array, display_counter[0]);
+		strip_1.post(strip_1_array);
+		convert_pixels(strip_2_array, display_counter[1]);
+		strip_2.post(strip_2_array);
+		convert_pixels(strip_3_array, display_counter[2]);
+		strip_3.post(strip_3_array);
+		convert_pixels(strip_4_array, display_counter[3]);
+		strip_4.post(strip_4_array);
 
+		stepper_motor.step(1);
+		display_counter[0]++;
+	}
 
 	return 1;
 }
-
